cbc605a44b18896b754f8b076d8e0ff87d7e54a8
Piyush Singh <85616154+piyushsinghoffice@users.noreply.github.com>
2023-06-27 15:38:39 +0530

    Update setup.py
    Updated library version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 with open("README.md", "r") as f:
     long_description = "\n" + f.read()
 
-VERSION = '0.0.0'
+VERSION = '0.0.1'
 
 # Short description
 DESCRIPTION = "LazyForecast is a Python library for performing univariate time series analysis using a lazy forecasting approach. This approach is designed to provide quick and simple forecasting models without requiring extensive configuration or parameter tuning."
